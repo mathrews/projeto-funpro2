15ff4b817dfd4557e8bf966d6fae3905cf36e6c0
distobs <cuppotatocake@gmail.com>
2025-06-24 18:13:20 -0300

    some stuff

--- esboco/scripts/grid.js
+++ esboco/scripts/grid.js
@@ -1,11 +1,52 @@
 const popups = document.querySelectorAll(".victory-modal-hidden");
+const cardContainer = document.querySelector(".card-container")
+
+let cards = [
+	"assets/coringa.jpg",
+	"assets/coringa.jpg",
+	"assets/coringa.jpg",
+	"assets/coringa.jpg",
+	"assets/coringa.jpg",
+	"assets/coringa.jpg",
+	"assets/coringa.jpg",
+	"assets/coringa.jpg",
+	"assets/coringa.jpg",
+	"assets/coringa.jpg",
+]
+
+window.onload = function() {
+	cards.forEach((e, index) => {
+		cardContainer.innerHTML += `
+		<div class="flip-card" onClick="trigger(${index})">
+					<div class="flip-card-inner" >
+						<div class="flip-card-front">
+							<h1>?</h1>
+						</div>
+						<div class="flip-card-back">
+							<img src="${e}"/>
+						</div>
+					</div>
+		</div>
+	`
+	})
+}
+
 function toggleWin() {
   	if (popups.length > 0) {
+		popups[0].id = "victory-modal";
+		popups[0].className = ""
   	}
 }
 
 function restart() {
   	if (popups.length > 0) {
     	popups[0].id = ""; 
+		popups[0].className = "victory-modal-hidden"
   	}
 }
+
+function trigger(num) {
+	let el = document.getElementsByClassName("flip-card-inner")
+	el[num].classList.add('flip-card-inner-animation')
+}
+
