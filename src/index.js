93ca3b476bf2448daa4bd219bc2edf90ce132ff7
mathrews <mateusps4gg@gmail.com>
2025-07-15 14:42:33 -0300

    adding adjustments for deploy in vercel

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@
 // inicializam o express e a constante da porta que a aplicação irá rodar.
 const express = require("express");
 const path = require('path')
+var cors = require('cors')
 const app = express();
 const port = 8080
 
 app.use(express.static(__dirname));// serve
 // arquivos estáticos
+
+app.use(cors())
 
 // lê o corpo (string) e converte p/ objeto
 app.use(express.urlencoded({ extended: true }));
